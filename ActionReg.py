5a8cd8a1f7469a8051ebefd3e00161fec79cc3ef
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-24 06:18:38 +0800

    Update ActionReg.py

--- ActionReg.py
+++ ActionReg.py
@@ -10,3 +10,10 @@ streamlit.title('Actions and Issues Tracker')
 #streamlit.header('Breakfast Menu')
 #streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 
+#test snowflake connection
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
