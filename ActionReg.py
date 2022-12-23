9a0a0d2d30b27d36cbba4dc0a295d0cf2200bf40
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-24 05:02:24 +0800

    Update ActionReg.py
    add initial code from first Streamlit app

--- ActionReg.py
+++ ActionReg.py
@@ -1,1 +1,15 @@
+# Import statements
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+#use this for Control of Flow changes - error message handling
+from urllib.error import URLError
 
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
