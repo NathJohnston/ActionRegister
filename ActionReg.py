6ec2a5b1e7724d8425b3943c5b9007980f50a258
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-25 04:56:29 +0800

    Update ActionReg.py

--- ActionReg.py
+++ ActionReg.py
@@ -14,7 +14,7 @@ streamlit.title('Actions and Issues Tracker')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT Truckclass FROM tblTruckPayloadTargets")
+my_cur.execute("SELECT * FROM tblTruckPayloadTargets")
 
 #my_data_row = my_cur.fetchone()
 my_data_rows = my_cur.fetchall()
