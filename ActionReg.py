6126120701b83f067759de115d48c8196087cae0
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-26 10:45:58 +0800

    Update ActionReg.py

--- ActionReg.py
+++ ActionReg.py
@@ -34,5 +34,5 @@ df = pd.DataFrame(
    np.random.randn(50, 20),
    columns=('col %d' % i for i in range(20)))
 
-streamlit.dataframe(df)  # Same as st.write(df)
+st.dataframe(df, 200, 100)
 # --------------------------------
