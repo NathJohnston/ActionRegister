4f3f101f9ccbfd4512be7da87347fd1a8fcc804e
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-26 10:46:53 +0800

    Update ActionReg.py

--- ActionReg.py
+++ ActionReg.py
@@ -19,8 +19,6 @@ my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * FROM tblTruckPayloadTargets")
 
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
 
 streamlit.header("Action/ Issue Register")
 #streamlit.dataframe(my_data_row)
@@ -28,11 +26,3 @@ streamlit.header("Action/ Issue Register")
 
 # new action variables
 action_date = streamlit.text_input('Action date:')
-
-# ----------Testing---------------
-df = pd.DataFrame(
-   np.random.randn(50, 20),
-   columns=('col %d' % i for i in range(20)))
-
-st.dataframe(df, 200, 100)
-# --------------------------------
